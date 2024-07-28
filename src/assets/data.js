6e531236f05c538f13441e8a94a1e9524651ed8d
RajdeepK18 <rajdeepkar41@gmail.com>
2024-07-28 05:33:51 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question:"Which country had won the Fifa World Cup 2022?",
+        option1: "France",
+        option2: "Brazil",
+        option3: "Argentina",
+        option4: "Germany",
+        ans: 3,
+    },
+    {
+        question:"Which continent has the highest number of countries?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question:"Who was the 1st President of India",
+        option1:"Pandit Jawaharlal Nehru",
+        option2:"Dr. Rajendra Prasad",
+        option3:"Sarvepalli Radhakrishnan",
+        option4:"Lal Bahadur Shastri",
+        ans:2,
+    },
+    {
+        question:"Against which bowler did Yuvraj Singh hit six consequtive sixes in an over during the inaugural T20 World Cup 2007?",
+        option1:"Andrew Flintoff",
+        option2:"James Anderson",
+        option3:"Stuart Broad",
+        option4:"Paul Collingwood",
+        ans:3,
+    },
+    {
+        question:"Which of these is not a principle of Object-Oriented Programming(OOPs)?",
+        option1:"Encapsulation",
+        option2:"Normalization",
+        option3:"Polymorphism",
+        option4:"Abstraction",
+        ans:2,
+    },
+];
